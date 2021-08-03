c3c7dc41dafa2b3a1c5ee40110973c20c0622cd6
seohee0112 <ghdtj7@naver.com>
2021-08-03 17:52:40 +0900

    [ADD] 산업분류 코드, 산업분류명칭 나눠서 리스트에 담기 추가

--- javascript/industryClassification.js
+++ javascript/industryClassification.js
@@ -1,120 +1,60 @@
-let industryA_Name = new Array();
-let industryA_Code = new Array();
+let industry_NameList = new Array(100);
+let industry_CodeList = new Array (100);
 
-let industryB_Name = new Array();
-let industryB_Code = new Array();
-
-let industryC_Name = new Array();
-let industryC_Code = new Array();
-
-let industryD_Name = new Array();
-let industryD_Code = new Array();
-
-let industryE_Name = new Array();
-let industryE_Code = new Array();
-
-let industryF_Name = new Array();
-let industryF_Code = new Array();
-
-let industryG_Name = new Array();
-let industryG_Code = new Array();
-
-let industryH_Name = new Array();
-let industryH_Code = new Array();
-
-let industryI_Name = new Array();
-let industryI_Code = new Array();
-
-let industryJ_Name = new Array();
-let industryJ_Code = new Array();
-
-let industryK_Name = new Array();
-let industryK_Code = new Array();
-
-let industryL_Name = new Array();
-let industryL_Code = new Array();
-
-let industryM_Name = new Array();
-let industryM_Code = new Array();
-
-let industryN_Name = new Array();
-let industryN_Code = new Array();
-
-let industryO_Name = new Array();
-let industryO_Code = new Array();
-
-let industryP_Name = new Array();
-let industryP_Code = new Array();
-
-let industryQ_Name = new Array();
-let industryQ_Code = new Array();
-
-let industryR_Name = new Array();
-let industryR_Code = new Array();
-
-let industryS_Name = new Array();
-let industryS_Code = new Array();
-
-let industryT_Name = new Array();
-let industryT_Code = new Array();
-
-let industryU_Name = new Array();
-let industryU_Code = new Array();
+//초기화
+for (let listIndex =1; listIndex < 100 ; listIndex++) {
+    industry_NameList[listIndex] = new Array();
+    industry_CodeList[listIndex] = new Array();
+}
 
 
-$(document).ready(function(){
-    $.ajax ({
-        type: "GET",
-        url: "https://api.odcloud.kr/api/15049591/v1/uddi:0ad240e1-1871-40e8-9ea9-aa54bbf6fca7",
-        data : "page=1&perPage=1936&returnType=XML&serviceKey=aqLTx6axfr2f%2FAZUjMxPpXKI3I1DNml6LJ9QY3lhzpDicnfJraWE6peuCCMdVKEsPY%2Bip5wD3wsx37zC2D6o5Q%3D%3D",
-        dataType : "XML",
-        success :function(response){
-            console.log(response);
+//파싱 시작
+$.ajax ({
+    type: "GET",
+    url: "https://api.odcloud.kr/api/15049591/v1/uddi:0ad240e1-1871-40e8-9ea9-aa54bbf6fca7",
+    data : "page=1&perPage=1936&returnType=XML&serviceKey=aqLTx6axfr2f%2FAZUjMxPpXKI3I1DNml6LJ9QY3lhzpDicnfJraWE6peuCCMdVKEsPY%2Bip5wD3wsx37zC2D6o5Q%3D%3D",
+    dataType : "XML",
+    success :function(response){
+        console.log(response);
 
-            xmlParsing(response); // 통신 성공시 호출
-        },
-        error: function (xhr, status, msg) { // 통신 실패시 호출해야하는 함수
-            console.log('상태값 : ' + status + ' Http에러메시지 : ' + msg);
-        },
-    });
+        // 통신 성공시 호출
+        pushIndustryList(response);    
+    },
+    error: function (xhr, status, msg) { // 통신 실패시 호출해야하는 함수
+        console.log('상태값 : ' + status + ' Http에러메시지 : ' + msg);
+    },
+});
 
-    // 번호별로 파싱시작
-    function xmlParsing(data) {
-        $(data).find("item").each(function(index, item){
-            let codeNumber = $(item).find('col[name=산업분류코드]').text();
-    
+//산업 분류 리스트 담기 
+function pushIndustryList(data) {
 
-            if (codeNumber.startsWith("01"))  {
-                
-               // console.log($(item).find('col[name=산업분류명칭]').text());
-            }   
-            else if (codeNumber.startsWith("02")) {
-                console.log($(item).find('col[name=산업분류명칭]').text());
-            }  
-            else if (codeNumber.startsWith("03")) {
+    //데이터 담을때, 필요한 인덱스
+    let currentlistIndex = 1;
+    let startCodeNumber = "01";
 
-            }  
-            else if (codeNumber.startsWith("04")) {
-
-            }  
-            else if (codeNumber.startsWith("05")) {
-
-            }  
-            else if (codeNumber.startsWith("06")) {
-
-            }  
-            else if (codeNumber.startsWith("07")) {
-
-            }  
-            else if (codeNumber.startsWith("08")) {
-
-            }  
-            else if (codeNumber.startsWith("09")) {
-
-            } 
-
-           
+    $(data).find("item").each(function(index, item){
+        let codeNumber = $(item).find('col[name=산업분류코드]').text();
         
-        })
-    }
-})
+        if (codeNumber.startsWith(startCodeNumber)) {
+            industry_NameList[currentlistIndex].push($(item).find('col[name=산업분류명칭]').text());
+            industry_CodeList[currentlistIndex].push($(item).find('col[name=산업분류코드]').text()) 
+        }
+        else {
+
+            startCodeNumber = parseInt(startCodeNumber);
+            startCodeNumber++;
+            if(startCodeNumber < 10) {
+                startCodeNumber = "0" + startCodeNumber;
+            }
+            else {
+                startCodeNumber += "";
+            }
+            currentlistIndex++;
+        }
+    
+    })
+}
+
+
+
+    
