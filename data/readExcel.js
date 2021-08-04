fef39614050a84950d4db44047100bc33f371005
nookcoder <gus5427@naver.com>
2021-08-05 01:00:12 +0900

    [ADD] 엑셀파일 추가

--- data/readExcel.js
+++ data/readExcel.js
@@ -82,22 +82,48 @@ const xlsx = require("xlsx");
 const express = require('excel');
 const FileReader = require('fs');
 
-var wb = xlsx.readFile("./2019/할당대상업체소재지.xlsx");
-var worksheet = wb.Sheets["Sheet0"];
-var json = xlsx.utils.sheet_to_json(worksheet);
+var firstWorkbook = xlsx.readFile("./할당대상업체/1차.xlsx");
+var secondWorkbook = xlsx.readFile("./할당대상업체/2차.xlsx");
+var thirdWorkbook = xlsx.readFile("./할당대상업체/3차.xlsx");
+var co2Workbook = xlsx.readFile("./2020/2020년도 업체별 명세서 주요정보_21.6.25기준(정정 최종).xlsx");
+
+var firstWorksheet = firstWorkbook.Sheets["Sheet0"];
+var secondWorksheet = secondWorkbook.Sheets["Sheet0"];
+var thirdWorksheet = thirdWorkbook.Sheets["Sheet0"];
+var co2Worksheet = co2Workbook.Sheets["명세서 주요정보(2020)_할당대상업체"]; 
+
+var fistJson = xlsx.utils.sheet_to_json(firstWorksheet);
+var secondJson = xlsx.utils.sheet_to_json(secondWorksheet);
+var thirdJson = xlsx.utils.sheet_to_json(thirdWorksheet);
+var co2Json = xlsx.utils.sheet_to_json(co2Worksheet);
 
 let company = new Array(); 
 
-function Info(year,name,location){
-    this.year = year; 
+function AssignmentCompany(period,name,location,agency,year,sector,co2,energy){
+    this.period = period; 
     this.name = name; 
     this.location = location; 
 }
 
-for(let index = 0; index<json.length; index++)
+for(let index = 0; index<fistJson.length; index++)
 {
-    let info = new Info(json[index].계획기간,json[index].업체명,json[index].소재지);
-    company.push(info);
+    let assignmentCompany = new AssignmentCompany(fistJson[index].계획기간,fistJson[index].업체명,fistJson[index].소재지);
+    company.push(assignmentCompany);
 }
-console.log(company[15].location);
+
+
+for(let index = 0; index<secondJson.length; index++)
+{
+    let assignmentCompany = new AssignmentCompany(secondJson[index].계획기간,secondJson[index].업체명,secondJson[index].소재지);
+    company.push(assignmentCompany);
+}
+
+
+for(let index = 0; index<thirdJson.length; index++)
+{
+    let assignmentCompany = new AssignmentCompany(thirdJson[index].계획기간,thirdJson[index].업체명,thirdJson[index].소재지);
+    company.push(assignmentCompany);
+}
+
+console.log(company.length);
 
