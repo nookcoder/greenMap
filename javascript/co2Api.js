1102a882dcfb4dd99b7217ed44a4d5bdd7a4fac1
seohee0112 <ghdtj7@naver.com>
2021-08-04 17:15:32 +0900

    [ADD] ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ

--- javascript/co2Api.js
+++ javascript/co2Api.js
@@ -1,5 +1,5 @@
-let serviceKey = "aqLTx6axfr2f/AZUjMxPpXKI3I1DNml6LJ9QY3lhzpDicnfJraWE6peuCCMdVKEsPY+ip5wD3wsx37zC2D6o5Q==";
-let numOfRows="100";
+let serviceKey = "aqLTx6axfr2f%2FAZUjMxPpXKI3I1DNml6LJ9QY3lhzpDicnfJraWE6peuCCMdVKEsPY%2Bip5wD3wsx37zC2D6o5Q%3D%3D";
+let numOfRows="10";
 let q1_year = "2018";
 let q2_area = "인천";
 let q3_peopleSize = "10인 ~ 19인";
@@ -7,14 +7,28 @@ let q4_industryCode= "28519";
 let q5_energyClassificationCode="전력";
 let q6_energyCode="전력";
 
+import express from 'express';
+const cors = require('cors');
+const app = express()
+ 
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 //파싱 시작
 $.ajax ({
+    //crossOrigin: true,
     type: "GET",
     url: "http://apis.data.go.kr/B553530/GHG_EMISSIONS_02/GHG_EMISSIONS_02_LIST",
-    data : "serviceKey="+serviceKey+"page=1&perPage="+ numOfRows  + "&q1=" + q1_year + "&q2=" + q2_area 
+    data : "serviceKey="+serviceKey+"&pageNo=1&numOfRows="+ numOfRows  + "&apiType=xml&q1=" + q1_year + "&q2=" + q2_area 
     + "&q3=" + q3_peopleSize + "&q4=" + q4_industryCode + "&q5=" +q5_energyClassificationCode
     + "&q6=" + q6_energyCode,
-    dataType : "XML",
     success :function(response){
         // 통신 성공시 호출
         console.log(response);    
