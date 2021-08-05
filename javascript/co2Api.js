8487b80e609c5e0a795b202f0d1eb88351b049a5
nookcoder <gus5427@naver.com>
2021-08-05 17:28:04 +0900

    [VERIFIED] 주석처리 잠깐할게요

--- javascript/co2Api.js
+++ javascript/co2Api.js
@@ -10,18 +10,18 @@ let q6_energyCode="전력";
 // import { createRequire } from '..node_modules/module';
 // const require = createRequire(import.meta.url);
 
-import express from '../node_modules/express/index.js';
-import cors from '../node_modules/cors/package.json';
+// import express from '../node_modules/express/index.js';
+// import cors from '../node_modules/cors/package.json';
 
-// const express = require('express');
-// const cors = require('cors');
-const app = express()
+// // const express = require('express');
+// // const cors = require('cors');
+// const app = express()
 
-app.use(cors())
+// app.use(cors())
  
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+// app.get('/products/:id', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
  
 app.listen(80, function () {
   console.log('CORS-enabled web server listening on port 80')
