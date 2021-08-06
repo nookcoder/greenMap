328cb61e4a55630d8dcc182e1464e51951bfd9c5
nookcoder <gus5427@naver.com>
2021-08-06 16:20:07 +0900

    [FIX[ 카카오맵으로 변경

--- javascript/companyMapApi.js
+++ javascript/companyMapApi.js
@@ -1,14 +1,9 @@
-$(document).ready(function() {
-	initTmap();
-});
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	level: 3 //지도의 레벨(확대, 축소 정도)
+};
 
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-function initTmap(){
-    var map = new Tmapv2.Map("company_map",  
-		{
-			center: new Tmapv2.LatLng(37.566481622437934,126.98502302169841), // 지도 초기 좌표
-			width: "100vw", 
-			height: "100vh",
-			zoom: 7
-	    });
-}
+//import assgin from './assignmentCompany.js';
