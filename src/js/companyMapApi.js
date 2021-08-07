b1e538d98e95530fa49c4f29b6551a3cbccb4a06
nookcoder <gus5427@naver.com>
2021-08-07 19:02:09 +0900

    [ADD] 할당업체 객체 반환 클래스 추가

--- src/js/companyMapApi.js
+++ src/js/companyMapApi.js
@@ -1,4 +1,3 @@
-
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
@@ -7,5 +6,6 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴;
 
-let json = require('./assignmentCompany.js');
-console.log(json.json[0].소재지);
+let companyData = require('./companyData.js');
+const ASSIGNMENT_COMPANY = companyData.assignmentCompany; 
+const TARGET_COMPANY = companyData.targetCompany;
