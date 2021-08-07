de12468a1d039aa5798b6964f1737ca9b53c85ff
seohee0112 <ghdtj7@gmail.com>
2021-08-07 23:51:09 +0900

    ㅇㅇㅇ

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports={
     mode : 'development',
     entry:{
@@ -12,7 +12,10 @@ module.exports={
     output : {
         filename:'[name]_bundle.js',
         path: path.resolve(__dirname, 'public'),
-    }
+    },
+    plugins: [
+        new NodePolyfillPlugin()
+    ]
 
     
 }
