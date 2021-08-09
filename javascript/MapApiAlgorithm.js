334d20031c1b6773a92b00b4dfeb50e49b0b9df4
jihoon98 <tlswlgns1003@sju.ac.kr>
2021-08-09 19:37:58 +0900

    [add]내일까지 ㅎㅇㅌ..

--- javascript/MapApiAlgorithm.js
+++ javascript/MapApiAlgorithm.js
@@ -1,6 +1,6 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 mapOption = { 
-    center: new kakao.maps.LatLng(36.007408855781904, 127.66055047039166), // 지도의 중심좌표
+    center: new kakao.maps.LatLng(36.007408855781904, 127.66055047039166), 
     level: 11
 };
 
--- javascript/areaMainMapApi.js
+++ javascript/areaMainMapApi.js
@@ -302,10 +302,11 @@ radio_4_1.addEventListener('click', function(){
       }})
       return marker;
   })
-
+  
     radio_2019.addEventListener('click', function(){
       var cnt=0;
       removeOverlay();
+      hoxy();
         const url='/data/지역별/2019년 온실가스.json';
         var cnt=0;
         fetch(url)
@@ -320,7 +321,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'>"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
           markers.push(marker);
           cnt++;
@@ -328,7 +329,8 @@ radio_4_1.addEventListener('click', function(){
       })
         return marker;
     })
-   
+
+    
 
     radio_2018.addEventListener('click', function(){ 
       var cnt=0;
@@ -348,7 +350,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div id='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
         markers.push(marker);
         }
@@ -375,7 +377,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+count+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
         markers.push(marker);
         }
@@ -402,7 +404,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+count+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
         markers.push(marker);
         }
@@ -430,7 +432,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+count+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
         markers.push(marker);
         }
@@ -458,7 +460,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+count+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
         markers.push(marker);
         }
@@ -483,7 +485,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+count+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
         markers.push(marker);
         }
@@ -510,7 +512,7 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+count+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
         markers.push(marker);
         }
@@ -522,10 +524,9 @@ radio_4_1.addEventListener('click', function(){
        
     })
     radio_2011.addEventListener('click', function(){
-      var cnt=0;
       removeOverlay();  
         const url='/data/지역별/2011년 온실가스.json';
-        
+        var cnt=0;
         fetch(url)
         .then((res)=>res.json())
         .then((myJson)=>{
@@ -538,8 +539,9 @@ radio_4_1.addEventListener('click', function(){
             var marker = new kakao.maps.CustomOverlay({
             map:map,
             position: latlng,
-            content:"<div class='ovr'"+count+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
+            content:"<div class='ovr'"+cnt+">"+region+'<br>'+gas+'tonCO2-eq'+"</div>"
           });
+          cnt++;
         markers.push(marker);
         }
         return marker;
@@ -547,3 +549,197 @@ radio_4_1.addEventListener('click', function(){
         });
     })
 } 
+
+
+function hoxy(){
+  var content = 'df';
+  var position = new kakao.maps.LatLng(33.450701, 126.570667); 
+  var seoul = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(37.566481622437934,126.98502302169841), 
+  radius: 21000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content:'123dfffffffffffffffffff'
+  }); 
+  var incheon = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(37.45617067506185, 126.70593716202376), 
+  radius: 14000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content       
+  }); 
+  var busan = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(35.1798978450028, 129.07504269074678), 
+  radius: 11000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content   
+  }); 
+  var ulsan = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(35.53931630388095, 129.31130011021926), 
+  radius: 16000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var gwangju = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(35.16028709174215, 126.8515187129608), 
+  radius: 7000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var daejeon = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(36.35064678347126, 127.38480796881625), 
+  radius: 10000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var gangwon = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(37.88677896568402, 127.72966438993677), 
+  radius: 18000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var gyeonggi = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(37.275102838671145, 127.00924374185942), 
+  radius: 25000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Jeonbuk = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(35.823678896579914, 127.10736956850765), 
+  radius: 12000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Jeonnam = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(34.81646521079705, 126.46295638411517), 
+  radius: 28000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Gyeongbuk = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(36.57775945486019, 128.5046014324505), 
+  radius: 20000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Gyeongnam = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(35.23899121204904, 128.6925696234592), 
+  radius: 12000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  text: '텍스트를 표시할 수 있어요!', 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Chungbuk = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(36.63646328354067, 127.49150542412117), 
+  radius: 12000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Chungnam = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(36.766068961948406, 126.71604722835751), 
+  radius: 30000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Jeju = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(33.66460246668256, 126.59726240332495), 
+  radius: 7000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var Sejong = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(36.48061467174786, 127.28876476267482), 
+  radius: 8000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  var daegu = new kakao.maps.Circle({
+  center : new kakao.maps.LatLng(35.87951572922946, 128.605608955793), 
+  radius: 9000, 
+  strokeWeight: 5, 
+  strokeColor: 'red',
+  strokeOpacity: 0.3, 
+  fillColor: 'red',
+  fillOpacity: 0.6,
+  content: content     
+  }); 
+  seoul.setMap(map);
+  daegu.setMap(map);
+  daejeon.setMap(map);
+  incheon.setMap(map);
+  busan.setMap(map);
+  ulsan.setMap(map);
+  gwangju.setMap(map);
+  gangwon.setMap(map);
+  gyeonggi.setMap(map);
+  Sejong.setMap(map);
+  Jeju.setMap(map);
+  Jeonbuk.setMap(map);
+  Jeonnam.setMap(map);
+  Chungbuk.setMap(map);
+  Chungnam.setMap(map);
+  Gyeongbuk.setMap(map);
+  Gyeongnam.setMap(map);
+  }
