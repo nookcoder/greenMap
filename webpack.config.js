1897a586472b57e500f04ac07059a5d271784256
nookcoder <gus5427@naver.com>
2021-08-09 23:43:55 +0900

    [FIX} 머지 충돌 수정

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,52 @@ var re = require('required-module');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode : "development",
-     
-    entry : {
-        companyMapiApiapi : "./src/js/companyMapApi.js",
-        companyPublicSection : "./src/js/companyPublicSection.js"
+module.exports={
+    mode : 'development',
+    entry:{
+         co2Api : "./src/js/co2Api.js",
+        industryMainSide : "./src/js/industryMainSideMenu.js",
+         industryClassification : "./src/js/industryClassification.js",
+         treeJs: "./src/js/treeJS.js"
+    },
+    output : {
+        filename: '[name].js',
+        path : path.resolve(__dirname,'public'),
+    },
+
+    module: {
+        
+        rules: [
+          { test: /\.xlsx$/, loader: "webpack-xlsx-loader" }
+        ],
+        noParse: [
+			/xlsx.core.min.js/,
+			/xlsx.full.min.js/
+		],
+        rules: [
+            {
+              test: /\.(png|jpe?g|gif)$/i,
+              use: [
+                {
+                  loader: 'file-loader',
+                },
+              ],
+            },
+          ],
+      },
+    
+      plugins: [
+        new NodePolyfillPlugin()
+    ]
+},
+
+module.exports={
+    mode : 'development',
+    entry:{
+         co2Api : "./src/js/co2Api.js",
+        industryMainSide : "./src/js/industryMainSideMenu.js",
+         industryClassification : "./src/js/industryClassification.js",
+         treeJs: "./src/js/treeJS.js"
     },
     output : {
         filename: '[name].js',
